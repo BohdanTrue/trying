8a575a46a65810d8712d8277589f90cfa4c72820
Bohdan Bilko <rapsbodik@gmail.com>
2023-07-15 17:53:54 +0300

    chinazes, i do my first Git Commit :)

---
+++ src/WorkingPeople.java
@@ -0,0 +1,22 @@
+public class WorkingPeople {
+    String name;
+    int age;
+    String work;
+    String placeAtWork;
+
+    public WorkingPeople(String name, int age, String work, String placeAtWork){
+        this.name = name;
+        this.age = age;
+        this.work = work;
+        this.placeAtWork = placeAtWork;
+    }
+
+    public void sayHello(){
+        System.out.println("Hi! My name is " + name + ", i'm " + age + " years old. I'm working in a" + work + " I have a good place: " + placeAtWork);
+    }
+    public static void main(String[] args) {
+        WorkingPeople person1 = new WorkingPeople("vasya", 43, "sirzavod", "gruz4ik");
+        person1.sayHello();
+    }
+}
+
