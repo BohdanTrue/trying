5d77a1d2ca6bc1a64ba6ddddcaca6bf32554febb
Bohdan Bilko <rapsbodik@gmail.com>
2023-07-15 17:56:20 +0300

    added new method sayHowOldAreYou()

--- src/WorkingPeople.java
+++ src/WorkingPeople.java
@@ -14,9 +14,14 @@ public class WorkingPeople {
     public void sayHello(){
         System.out.println("Hi! My name is " + name + ", i'm " + age + " years old. I'm working in a" + work + " I have a good place: " + placeAtWork);
     }
+
+    public void sayHowOldAreYou(){
+        System.out.println("I'm " + age + " years old");
+    }
     public static void main(String[] args) {
         WorkingPeople person1 = new WorkingPeople("vasya", 43, "sirzavod", "gruz4ik");
         person1.sayHello();
+        person1.sayHowOldAreYou();
     }
 }
 
